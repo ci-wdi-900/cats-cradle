b08e48b756b8f5a6400387bbe47c999e76e75a73
mesuara <mesuara@gmail.com>
2020-02-13 10:37:44 -0500

    deleted the extra files

--- main.test.js
+++
@@ -1,9 +0,0 @@
-const {
-  exampleFunction,
-} = require('./main.js')
-
-describe('exampleFunction', () => {
-  it(`does nothing, so... you should probably change this test`, () => {
-    expect(exampleFunction()).toBe(undefined)
-  })
-})
